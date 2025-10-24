02061c0a3424eb84ebe1a5f857f1dc912d4cbecc
Julczan <julek111222h@gmail.com>
2025-10-24 13:50:01 +0200

    removing duplicates and sorting an array

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -5,3 +5,24 @@ class Node {
     this.right = right;
   }
 }
+
+class Tree {
+  constructor(array) {
+    this.array = array;
+    this.root = this.buildTree(this.array);
+  }
+
+  buildTree(array) {
+    let uniq = [...new Set(array)];
+    let sortedArr = uniq.sort((a, b) => {
+      return a - b;
+    });
+    return sortedArr;
+  }
+}
+
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const test = new Tree(arr);
+
+console.log(test.root);
