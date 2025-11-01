500cdf4ee29cf84c7d3d217f584c3db393f56fd5
Julczan <julek111222h@gmail.com>
2025-11-01 20:18:06 +0100

    using npm

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -222,14 +222,4 @@ class Tree {
   }
 }
 
-// const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const arr = [1, 2, 3, 4, 5, 6, 7];
-
-const test = new Tree(arr);
-
-test.insert(8);
-test.insert(9);
-
-test.rebalance();
-
-test.prettyPrint(test.root);
+export default Tree;
---
+++ driver-script.js
@@ -0,0 +1,7 @@
+import Tree from "./binary-search-tree.js";
+
+const arr = [1, 2, 3, 4, 5];
+
+const test = new Tree(arr);
+
+test.prettyPrint(test.root);
